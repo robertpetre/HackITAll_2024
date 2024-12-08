00ca071a33d5de4feeac3e4055d81ad57182921e
ReGeLePuMa <scpetrea@gmail.com>
2024-12-08 03:12:04 +0200

    Added frontend route

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,26 +1,39 @@
 from flask import jsonify, request, Blueprint
 from pymongo import *
+from collections import defaultdict
+from .utils import MongoSingleton
 
-
-MONGO_URL = 'mongodb://db:27017/'
-DB_NAME = 'hackitall'
-NEWS_COL = 'news'   
-
-client = MongoClient(MONGO_URL)
-db = client[DB_NAME]
-news = db[NEWS_COL]
+news_col = MongoSingleton.get_collection("news")
+tags_col = MongoSingleton.get_collection("tags")
 
 
 routes = Blueprint('app', __name__)
 
 @routes.route('/', methods=['GET'])
 def index():
-    return jsonify({'message': 'Hello, World!'})
+    tags_list = []
+    tags_mean_map = defaultdict(list)
+    tags_title_map = defaultdict(list)
+    for tags in tags_col.find():
+        print(tags)
+        entry =  {}
+        entry["tag"] = tags["tag"]
+        news = news_col.find_one({"_id": tags["news_id"]})
+        tags_mean_map[entry["tag"]].append(news["sentimentalScore"])
+        tags_title_map[entry["tag"]].append((news["title"], news["isGoodNews"], news["link"]))
+    for tag in tags_mean_map:
+        entry = {}
+        entry["tag"] = tag
+        entry["mean"] = sum(tags_mean_map[tag]) / len(tags_mean_map[tag])
+        entry["news"] = tags_title_map[tag]
+        tags_list.append(entry)
+    return jsonify(tags_list)
 
 @routes.route('/health', methods=['GET'])
 def health():
     try:
-        client.admin.command('ping')  
+        client = MongoSingleton.get_client("news")
+        client.server_info()  
     except Exception as e:
         return jsonify({'message': 'Error: ' + str(e)}), 500
     return jsonify({'message': 'OK'})   
